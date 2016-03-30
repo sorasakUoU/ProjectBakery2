e74843c3d9d0837373a8f26662dc4c3c1b5cc420
sorasakUoU <nightontherock@gmail.com>
2016-03-30 09:15:22 +0700

    Context Class

--- app/src/main/java/kawinpart/sorasak/buybakery/MainActivity.java
+++ app/src/main/java/kawinpart/sorasak/buybakery/MainActivity.java
@@ -4,18 +4,46 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-
-
+    //Explicit
+    private EditText userEditText, passwordEditText;
+    private String userString, passwordString;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Bind Widget
+        BindWidget();
+
     } //Main Method
+
+    private void BindWidget() {
+        userEditText = (EditText) findViewById(R.id.txtUser);
+        passwordEditText = (EditText) findViewById(R.id.txtPassword);
+    }
+
+    public void clickSingInMain(View view) {
+        userString = userEditText.getText().toString().trim();
+        passwordString = passwordEditText.getText().toString().trim();
+
+        //Check Space
+        if (userString.equals("") || passwordString.equals("")) {
+            //Have Space
+            myAlert("โปรดกรอกให้ ครบทุกช่องค่ะ");
+        }
+
+    } // ClickSignInMain
+
+    private void myAlert(String strAlert) {
+
+    }
 
     public void clickSignUpMain(View view) {
         startActivity(new Intent(MainActivity.this, SignUpActivity.class));
---
+++ app/src/main/java/kawinpart/sorasak/buybakery/MyAlertDialog.java
@@ -0,0 +1,15 @@
+package kawinpart.sorasak.buybakery;
+
+import android.content.Context;
+
+/**
+ * Created by NekokanSama on 30/3/2559.
+ */
+public class MyAlertDialog {
+
+    public void myDialog(Context context,
+                         String strTitle,
+                         String strMessage) {
+
+    }
+} // Main Class
